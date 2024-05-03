2da1e9078841673c44a6b97ce33d007cea10b014
David Kwon <dkwon1223@gmail.com>
2024-05-03 01:14:29 -0600

    Rename new component for dynamic reusability

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx'
-import Sports from './components/Sports.jsx';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import ArticleCategory from "./components/ArticleCategory.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />}/>
-        <Route path='/sports' element={<Sports />}/>
+        <Route path="/" element={<App />} />
+        <Route path="/sports" element={<ArticleCategory />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

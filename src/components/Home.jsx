b2165909c8f9212292b980bcaea2292bc824ebcd
David Kwon <dkwon1223@gmail.com>
2024-05-02 23:59:44 -0600

    Add thicker border to cards containers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,14 +20,14 @@ const Home = () => {
         <h1 className="font-bold text-5xl text-indigo-600 mb-6">Recent News</h1>
         <section
         id="recentNewsContainer"
-        className="h-[60vh] w-[95%] py-12 px-12 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto border rounded-xl shadow-2xl mb-12"
+        className="h-[60vh] w-[95%] py-12 px-12 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto border-4 rounded-xl border-indigo-600 shadow-2xl mb-12"
         >
             {recentNewsArticles}
         </section>
         <h1 className="font-bold text-5xl mb-6 text-indigo-600">Top Stories</h1>
         <section
         id="recentNewsContainer"
-        className="h-[60vh] w-[95%] py-12 px-12 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto border rounded-xl shadow-2xl mb-12"
+        className="h-[60vh] w-[95%] py-12 px-12 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto border-4 rounded-xl border-indigo-600 shadow-2xl mb-12"
         >
             {recentNewsArticles}
         </section>

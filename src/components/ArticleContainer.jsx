bf99659ad4f11e5841624397e929d595379b0bdd
David Kwon <dkwon1223@gmail.com>
2024-05-03 00:10:26 -0600

    Refactor with ArticleContainer component for reusability

---
+++ src/components/ArticleContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ArticleContainer = ({ articles, height }) => {
+  return (
+    <>
+      <h1 className="font-bold text-5xl text-indigo-600 mb-6">Recent News</h1>
+      <section
+        id="recentNewsContainer"
+        className={`h-[${height}vh] w-[95%] py-12 px-12 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto border-4 rounded-xl border-indigo-600 shadow-2xl mb-12`}
+      >
+        {articles}
+      </section>
+    </>
+  );
+};
+
+export default ArticleContainer;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { articles } from "../mockData";
 import Card from "./Card";
+import ArticleContainer from "./ArticleContainer";
 
 const Home = () => {
   const recentNewsArticles = articles.map((article, index) => {
@@ -17,20 +18,8 @@ const Home = () => {
 
   return (
     <section className="flex flex-col items-center py-6">
-        <h1 className="font-bold text-5xl text-indigo-600 mb-6">Recent News</h1>
-        <section
-        id="recentNewsContainer"
-        className="h-[60vh] w-[95%] py-12 px-12 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto border-4 rounded-xl border-indigo-600 shadow-2xl mb-12"
-        >
-            {recentNewsArticles}
-        </section>
-        <h1 className="font-bold text-5xl mb-6 text-indigo-600">Top Stories</h1>
-        <section
-        id="recentNewsContainer"
-        className="h-[60vh] w-[95%] py-12 px-12 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-3 xl:gap-x-8 overflow-y-auto border-4 rounded-xl border-indigo-600 shadow-2xl mb-12"
-        >
-            {recentNewsArticles}
-        </section>
+        <ArticleContainer articles={recentNewsArticles} height="60"/>
+        <ArticleContainer articles={recentNewsArticles} height="60"/>
     </section>
   );
 };
